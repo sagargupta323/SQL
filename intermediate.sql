a851dc105c429ce64f26a857f1ab776b9ce761d6
sagargupta323 <67185306+sagargupta323@users.noreply.github.com>
2022-06-09 17:02:09 +0530

    Create intermediate.sql

---
+++ intermediate.sql
@@ -0,0 +1,48 @@
+Table1  
+
+| COL A |
+|-------|
+| 1     |
+| 1     |
+| 1     |
+
+Table2
+
+| COL A |
+|-------|
+| 1     |
+| 1     |
+
+What is the output of the following query?
+1.  select * from table1
+    Left join table2 
+    on table1.col_A=table2.col_A
+    
+2.  select * from table1
+    inner join table2 
+    on table1.col_A=table2.col_A
+    
+3.  select * from table1
+    right join table2 
+    on table1.col_A=table2.col_A
+    
+    
+| col_A | col_A |
+|-------|-------|
+| 1     | 1     |
+| 1     | 1     |
+| 1     | 1     |
+| 1     | 1     |
+| 1     | 1     |
+| 1     | 1     |
+| 1     | 1     |
+| 1     | 1     |
+| 1     | 1     |
+
+
+
+
+
+    
+    
+    
