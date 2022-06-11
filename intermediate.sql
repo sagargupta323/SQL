612707f05e290dfb3d86503b94b516097960b9fd
sagargupta323 <67185306+sagargupta323@users.noreply.github.com>
2022-06-11 22:33:01 +0530

    Update intermediate.sql

--- intermediate.sql
+++ intermediate.sql
@@ -1,17 +1,11 @@
-Table1  
+Table1         Table2
 
-| COL A |
-|-------|
-| 1     |
-| 1     |
-| 1     |
+| COL A |     | COL A |
+|-------|     |-------|
+| 1     |     | 1     |
+| 1     |     | 1     |
+| 1     |     | 1     |
 
-Table2
-
-| COL A |
-|-------|
-| 1     |
-| 1     |
 
 What is the output of the following query?
 1.  select * from table1
@@ -26,7 +20,8 @@ What is the output of the following query?
     right join table2 
     on table1.col_A=table2.col_A
     
-    
+ANSWER: 
+
 | col_A | col_A |
 |-------|-------|
 | 1     | 1     |
@@ -38,6 +33,25 @@ What is the output of the following query?
 | 1     | 1     |
 | 1     | 1     |
 | 1     | 1     |
+
+
+/*
+An eCommerce company's online order table.
+
+  col_name    | col_type
+--------------+-------------------
+order_id      | bigint
+product_id    | bigint
+customer_id   | bigint
+order_dt      | date
+qty           | integer
+unit_price_usd| float
+channel       | varchar(20) -- mobile, desktop  
+
+Write a query to return the top 3 and bottom 3 products this month ranked by sales.
+sales = sum(unit_price_usd * qty)
+*/
+
 
 
 
