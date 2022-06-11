edf356d46c38bfdb12af375e73859b13c11fd3ed
sagargupta323 <67185306+sagargupta323@users.noreply.github.com>
2022-06-11 22:28:27 +0530

    Update Level0.sql

--- Level0.sql
+++ Level0.sql
@@ -1,3 +1,10 @@
+/*---------------------------------NOTES-----------------------------------------------------
+1. You can use window functions in SELECT and ORDER BY. 
+   You cannot use window functions in WHERE, GROUP BY, or HAVING. Use a subquery or a WITH query to get around this limitation.
+   
+
+------------------------------------------------------------------------------------------*/
+
 -- Write a SQL query to find the nth highest salary from employee table. 
 -- Example: finding 3rd highest salary from employee table
 
@@ -6,18 +13,12 @@ FROM employees
 ORDER by Salary desc
 LIMIT 1,2;
 
-
 -- OR 
 
-SELECT 
-    emp_id,
-    salary
-FROM
-    employees
-ORDER BY 
-	salary DESC
+SELECT emp_id,salary
+FROM employees
+ORDER BY salary DESC
 LIMIT 1 OFFSET 2;
-
 
 
 
